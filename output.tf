80f65ce8a3beb519a753a6197386a134eaf5ac03
jasleendhir11 <jkourdhir@gmail.com>
2025-07-11 00:39:52 -0400

    docs: add inline comments for the resource group name

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
+# Outputs the name of the resource group
+
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
 
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
