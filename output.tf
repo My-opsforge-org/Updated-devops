1854c0be5ec8137d13853386f037b5e679e8075e
jasleendhir11 <jkourdhir@gmail.com>
2025-07-11 00:42:17 -0400

    docs: add public IP of the VM

--- output.tf
+++ output.tf
@@ -4,6 +4,7 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+# Outputs the public IP of the VM
 
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
